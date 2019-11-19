e293f8b43c47140c131069f9b91c5e3f04124a2a
pengyuanlong <pengyuanlong@xrd-lcd.com.com>
2019-11-19 10:59:16 +0800

    第一次提交

---
+++ mylibrary/src/main/java/com/xrd/mylibrary/DataUtils.java
@@ -0,0 +1,304 @@
+package com.xrd.mylibrary;
+
+import java.math.BigInteger;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 串口数据转换工具类
+ * Created by Administrator on 2019/2/25.
+ */
+public class DataUtils {
+    //-------------------------------------------------------
+    // 判断奇数或偶数，位运算，最后一位是1则为奇数，为0是偶数
+    public static int isOdd(int num) {
+        return num & 1;
+    }
+
+    //-------------------------------------------------------
+    //Hex字符串转int
+    public static int HexToInt(String inHex) {
+        return Integer.parseInt(inHex, 16);
+    }
+
+    public static String IntToHex(int intHex) {
+        return Integer.toHexString(intHex);
+    }
+
+    //-------------------------------------------------------
+    //Hex字符串转byte
+    public static byte HexToByte(String inHex) {
+        return (byte) Integer.parseInt(inHex, 16);
+    }
+
+    //-------------------------------------------------------
+    //1字节转2个Hex字符
+    public static String Byte2Hex(Byte inByte) {
+        return String.format("%02x", new Object[]{inByte}).toUpperCase();
+    }
+
+    //-------------------------------------------------------
+    //字节数组转转hex字符串
+    public static String ByteArrToHex(byte[] inBytArr) {
+        StringBuilder strBuilder = new StringBuilder();
+        for (byte valueOf : inBytArr) {
+            strBuilder.append(Byte2Hex(Byte.valueOf(valueOf)));
+            strBuilder.append(" ");
+        }
+        return strBuilder.toString();
+    }
+
+    //-------------------------------------------------------
+    //字节数组转转hex字符串，可选长度
+    public static String ByteArrToHex(byte[] inBytArr, int offset, int byteCount) {
+        StringBuilder strBuilder = new StringBuilder();
+        int j = byteCount;
+        for (int i = offset; i < j; i++) {
+            strBuilder.append(Byte2Hex(Byte.valueOf(inBytArr[i])));
+        }
+        return strBuilder.toString();
+    }
+    /**
+     * 十六进制字节数组转字符串
+     *
+     * @param src 目标数组
+     * @param dec 起始位置
+     * @param length 长度
+     * @return
+     */
+    public static String bytes2HexStr(byte[] src, int dec, int length) {
+        byte[] temp = new byte[length];
+        System.arraycopy(src, dec, temp, 0, length);
+        return bytes2HexStr(temp);
+    }
+    /**
+     * 字节数组转换成对应的16进制表示的字符串
+     *
+     * @param src
+     * @return
+     */
+    public static String bytes2HexStr(byte[] src) {
+        StringBuilder builder = new StringBuilder();
+        if (src == null || src.length <= 0) {
+            return "";
+        }
+        char[] buffer = new char[2];
+        for (int i = 0; i < src.length; i++) {
+            buffer[0] = Character.forDigit((src[i] >>> 4) & 0x0F, 16);
+            buffer[1] = Character.forDigit(src[i] & 0x0F, 16);
+            builder.append(buffer);
+        }
+        return builder.toString().toUpperCase();
+    }
+    //-------------------------------------------------------
+    //转hex字符串转字节数组
+    public static byte[] HexToByteArr(String inHex) {
+        byte[] result;
+        int hexlen = inHex.length();
+        if (isOdd(hexlen) == 1) {
+            hexlen++;
+            result = new byte[(hexlen / 2)];
+            inHex = "0" + inHex;
+        } else {
+            result = new byte[(hexlen / 2)];
+        }
+        int j = 0;
+        for (int i = 0; i < hexlen; i += 2) {
+            result[j] = HexToByte(inHex.substring(i, i + 2));
+            j++;
+        }
+        return result;
+    }
+
+
+    /**
+     * 十六进制转化十进制，带符号
+     *
+     * @param
+     * @return
+     */
+    public static int Hex2ToTempera(String s) {
+        int parseInt = Integer.parseInt(s, 16);
+        if (parseInt < 32768) return parseInt;
+        return parseInt - 65536;
+    }
+
+
+    /**
+     * 按照指定长度切割字符串
+     *
+     * @param inputString 需要切割的源字符串
+     * @param length      指定的长度
+     * @return
+     */
+    public static List<String> getDivLines(String inputString, int length) {
+        List<String> divList = new ArrayList<>();
+        int remainder = (inputString.length()) % length;
+        // 一共要分割成几段
+        int number = (int) Math.floor((inputString.length()) / length);
+        for (int index = 0; index < number; index++) {
+            String childStr = inputString.substring(index * length, (index + 1) * length);
+            divList.add(childStr);
+        }
+        if (remainder > 0) {
+            String cStr = inputString.substring(number * length, inputString.length());
+            divList.add(cStr);
+        }
+        return divList;
+    }
+
+    /**
+     * 计算长度，两个字节长度
+     *
+     * @param val value
+     * @return 结果
+     */
+    public static String twoByte(String val) {
+        if (val.length() > 4) {
+            val = val.substring(0, 4);
+        } else {
+            int l = 4 - val.length();
+            for (int i = 0; i < l; i++) {
+                val = "0" + val;
+            }
+        }
+        return val;
+    }
+
+    /**
+     * 校验和
+     *
+     * @param cmd 指令
+     * @return 结果
+     */
+    public static String sum(String cmd) {
+        List<String> cmdList = DataUtils.getDivLines(cmd, 2);
+        int sumInt = 0;
+        for (String c : cmdList) {
+            sumInt += DataUtils.HexToInt(c);
+        }
+        String sum = DataUtils.IntToHex(sumInt);
+        sum = DataUtils.twoByte(sum);
+        cmd += sum;
+        return cmd.toUpperCase();
+    }
+
+    /**
+     * 将byte[]转为各种进制的字符串
+     *
+     * @param bytes byte[]
+     * @param radix 基数可以转换进制的范围，从Character.MIN_RADIX到Character.MAX_RADIX，超出范围后变为10进制
+     * @return 转换后的字符串
+     */
+    public static String ByteArrToBinary(byte[] bytes, int radix) {
+        return new BigInteger(1, bytes).toString(radix);// 这里的1代表正数
+    }
+
+    /**
+     *  十六進制轉ASCII
+     * @param
+     * @return
+     */
+
+    public static String hex2ASCII(String hex) {
+        StringBuilder sb = new StringBuilder();
+        StringBuilder temp = new StringBuilder();
+        //49204c6f7665204a617661 split into two characters 49, 20, 4c...
+        for (int i = 0; i < hex.length() - 1; i += 2) {
+            //grab the hex in pairs
+            String output = hex.substring(i, (i + 2));
+            //convert hex to decimal
+            int decimal = Integer.parseInt(output, 16);
+            //convert the decimal to character
+            sb.append((char) decimal);
+            temp.append(decimal);
+        }
+        return sb.toString();
+    }
+
+
+    public static String getCRC2(byte[] bytes) {
+//        ModBus 通信协议的 CRC ( 冗余循环校验码含2个字节, 即 16 位二进制数。
+//        CRC 码由发送设备计算, 放置于所发送信息帧的尾部。
+//        接收信息设备再重新计算所接收信息 (除 CRC 之外的部分）的 CRC,
+//        比较计算得到的 CRC 是否与接收到CRC相符, 如果两者不相符, 则认为数据出错。
+//
+//        1) 预置 1 个 16 位的寄存器为十六进制FFFF(即全为 1) , 称此寄存器为 CRC寄存器。
+//        2) 把第一个 8 位二进制数据 (通信信息帧的第一个字节) 与 16 位的 CRC寄存器的低 8 位相异或, 把结果放于 CRC寄存器。
+//        3) 把 CRC 寄存器的内容右移一位( 朝低位)用 0 填补最高位, 并检查右移后的移出位。
+//        4) 如果移出位为 0, 重复第 3 步 ( 再次右移一位); 如果移出位为 1, CRC 寄存器与多项式A001 ( 1010 0000 0000 0001) 进行异或。
+//        5) 重复步骤 3 和步骤 4, 直到右移 8 次,这样整个8位数据全部进行了处理。
+//        6) 重复步骤 2 到步骤 5, 进行通信信息帧下一个字节的处理。
+//        7) 将该通信信息帧所有字节按上述步骤计算完成后,得到的16位CRC寄存器的高、低字节进行交换。
+//        8) 最后得到的 CRC寄存器内容即为 CRC码。
+
+        int CRC = 0x0000ffff;
+        int POLYNOMIAL = 0x0000a001;
+
+        int i, j;
+        for (i = 0; i < bytes.length; i++) {
+            CRC ^= ((int) bytes[i] & 0x000000ff);
+            for (j = 0; j < 8; j++) {
+                if ((CRC & 0x00000001) != 0) {
+                    CRC >>= 1;
+                    CRC ^= POLYNOMIAL;
+                } else {
+                    CRC >>= 1;
+                }
+            }
+        }
+        //高低位转换，看情况使用（譬如本人这次对led彩屏的通讯开发就规定校验码高位在前低位在后，也就不需要转换高低位)
+        CRC = ((CRC & 0x0000FF00) >> 8) | ((CRC & 0x000000FF) << 8);
+        String s = Integer.toHexString(CRC);
+        if (s.length() == 3) return "0" + s;
+        if (s.length() == 2) return "00" + s;
+        if (s.length() == 1) return "000" + s;
+        return s;
+    }
+
+    public static String hexString2binaryString(String hexString) {
+        if (hexString == null || hexString.length() % 2 != 0)
+            return null;
+        String bString = "", tmp;
+        for (int i = 0; i < hexString.length(); i++) {
+            tmp = "0000" + Integer.toBinaryString(Integer.parseInt(hexString.substring(i, i + 1), 16));
+            bString += tmp.substring(tmp.length() - 4);
+        }
+        return bString;
+    }
+
+    //------------------------------------------------------
+    public static String binaryString2hexString(String bString) {
+        if (bString == null || bString.equals("") || bString.length() % 8 != 0)
+            return null;
+        StringBuffer tmp = new StringBuffer();
+        int iTmp = 0;
+        for (int i = 0; i < bString.length(); i += 4) {
+            iTmp = 0;
+            for (int j = 0; j < 4; j++) {
+                iTmp += Integer.parseInt(bString.substring(i + j, i + j + 1)) << (4 - j - 1);
+            }
+            tmp.append(Integer.toHexString(iTmp));
+        }
+        return tmp.toString();
+    }
+
+
+    public static String currentTime() {
+        long currentTime = System.currentTimeMillis();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy.MM.dd,HH:mm:ss");
+        Date date = new Date(currentTime);
+        String nowDate = formatter.format(date);
+        return nowDate;
+    }
+    public static String currentTime(long time) {
+//        SimpleDateFormat formatter = new SimpleDateFormat("yyyy.MM.dd,HH:mm:ss");
+        SimpleDateFormat formatter = new SimpleDateFormat("MM.dd,HH:mm");
+        Date date = new Date(time);
+        String nowDate = formatter.format(date);
+        return nowDate;
+    }
+
+}
